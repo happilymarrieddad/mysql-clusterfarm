a864dfbc8eb96c32d83c903d076d0561d57e01de
Nick Kotenberg <nkotenberg@fusiontms.com>
2016-09-07 10:23:59 -0600

    finally completed an initial build

---
+++ test/unit/pool-cluster/test-connection-order-fatal.js
@@ -0,0 +1,41 @@
+var after   = require('after');
+var assert  = require('assert');
+var common  = require('../../common');
+var cluster = common.createPoolCluster();
+var server  = common.createFakeServer();
+
+var poolConfig1 = common.getTestConfig({port: common.bogusPort});
+var poolConfig2 = common.getTestConfig({port: common.fakeServerPort});
+cluster.add('SLAVE1', poolConfig1);
+cluster.add('SLAVE2', poolConfig2);
+
+server.listen(common.fakeServerPort, function(err) {
+  assert.ifError(err);
+
+  var conn1;
+  var conn2;
+  var pool = cluster.of('SLAVE*', 'ORDER');
+
+  var done = after(2, function () {
+    cluster.end(function (err) {
+      assert.ifError(err);
+      server.destroy();
+    });
+  });
+
+  pool.getConnection(function (err, connection) {
+    assert.ifError(err);
+    assert.strictEqual(connection._clusterId, 'SLAVE2');
+
+    conn1 = connection;
+    done();
+  });
+
+  pool.getConnection(function (err, connection) {
+    assert.ifError(err);
+    assert.strictEqual(connection._clusterId, 'SLAVE2');
+
+    conn2 = connection;
+    done();
+  });
+});
